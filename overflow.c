7c792f0126b4c3648f5e41d350ce2e088c809b77
edsnyder <edsnyder@mitre.org>
2019-07-10 09:26:21 -0400

    increased difficulty

--- overflow.c
+++ overflow.c
@@ -9,9 +9,17 @@ void win(){
     system("/bin/sh");
 }
 
+int check(char * buffer){
+    if(buffer[0] == '0'){
+        return 1;
+    }
+    return 0;
+}
+
 int main(int argc, char** argv){
     char buffer[32];
     printf("Can you figure out how to overflow the buffer?? (y/n)\n");
     gets(buffer);
+    check(buffer);
     return 0;
 }
